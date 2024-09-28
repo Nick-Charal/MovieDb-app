fc914bb1ef294da32793b18be50d023ca702d3d9
Nick-Charal <nick.charalampop@gmail.com>
2024-09-28 20:31:17 +0300

    Commit

--- app/search/[searchId]/page.tsx
+++ app/search/[searchId]/page.tsx
@@ -1,7 +1,7 @@
 import Results from '@/components/Results';
 import React from 'react'
 
-export default async function searchPage({params}) {
+export default async function searchPage({ params }) {
     const searchId = params.searchId;
     const resp = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${searchId}&page=1`);
     const data = await resp.json();
