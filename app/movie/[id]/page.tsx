ff44ba758a64422d0e255f88b0983cc0af0489b9
Nick-Charal <129396235+Nick-Charal@users.noreply.github.com>
2024-09-30 11:14:25 +0300

    Update page.tsx

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image';
-import Link from 'next/link';
+// import Link from 'next/link';
 
 function getImageUrl(path: string) {
   return (`https://image.tmdb.org/t/p/original/${path}`)
