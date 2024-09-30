356c33bf423e58fff5dd8ba984feeee88f60ba2c
Nick-Charal <129396235+Nick-Charal@users.noreply.github.com>
2024-09-30 11:43:44 +0300

    Update page.tsx

--- app/search/[searchId]/page.tsx
+++ app/search/[searchId]/page.tsx
@@ -1,23 +1,23 @@
 import Results from '@/components/Results';
 import React from 'react'
 
-export async function generateStaticParams() {
-  // const resp = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.API_KEY}&language=en-US&page=1`);
-  // const data = await resp.json();
-  // const results = data.results;
+// export async function generateStaticParams() {
+//   // const resp = await fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.API_KEY}&language=en-US&page=1`);
+//   // const data = await resp.json();
+//   // const results = data.results;
 
-  // if (!resp.ok) {
-  //   throw new Error('Something went wrong');
-  // }
+//   // if (!resp.ok) {
+//   //   throw new Error('Something went wrong');
+//   // }
 
-  // return results.map((movie => {
-  //   searchId: movie.id
-  // }));
-  return [
-    {searchId: '238'},
-    {searchId: '240'}
-  ]
-}
+//   // return results.map((movie => {
+//   //   searchId: movie.id
+//   // }));
+//   return [
+//     {searchId: '238'},
+//     {searchId: '240'}
+//   ]
+// }
 
 export default async function searchPage({ params }) {
   const searchId = params.searchId;
